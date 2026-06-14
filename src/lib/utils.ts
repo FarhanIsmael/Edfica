8a8cfd60a5622c337715d7543421d9f060e8639b
FarhanIsmael <145181283+FarhanIsmael@users.noreply.github.com>
2026-06-14 01:03:28 -0700

    Remove hardcoded currency helpers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) { return twMerge(clsx(inputs)); }
-
-export const formatCurrency = (value: number, currency = "KES") => new Intl.NumberFormat("en-KE", { style: "currency", currency, maximumFractionDigits: 0 }).format(value);
+export const number = (value: number) => new Intl.NumberFormat("en-US").format(value);
+export const percent = (value: number) => `${value.toFixed(value % 1 ? 1 : 0)}%`;
+export const amount = (value: number) => new Intl.NumberFormat("en-US", { maximumFractionDigits: 0 }).format(value);
